cc7898175156f5c279d92df0c3d198115bdc4b7a
Jan Sobielarski <jsobielarski@gmail.com>
2019-03-18 15:20:23 +0100

    Upload urls app_name added

--- Disc_forum/upload/urls.py
+++ Disc_forum/upload/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from upload.views import DocumentCreateView
 
-app_name = 'main'
+app_name = 'upload'
 
 urlpatterns = [
     path('upload/', DocumentCreateView.as_view(), name='upload'),
