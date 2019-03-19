67c1928aab9a4315e4b01b6ebb9e6029508f0e59
Jan Sobielarski <jsobielarski@gmail.com>
2019-03-19 14:42:02 +0100

    Document model

--- Disc_forum/upload/models.py
+++ Disc_forum/upload/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Document(models.Model):
+    """
+    Model for uploaded documents - description, path to document, upload time
+    """
+    description = models.CharField(max_length=255, blank=True)
+    document = models.FileField(upload_to='documents/')
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
