319c5c45a398c8d0660a3a2fe3adbe8c49730045
Jan Sobielarski <jsobielarski@gmail.com>
2019-03-21 15:34:46 +0100

    DocumentCreateView

--- Disc_forum/upload/views.py
+++ Disc_forum/upload/views.py
@@ -1,7 +1,19 @@
-from django.views import View
+from rest_framework import generics
+
+from upload.models import Document
+from upload.serializers import DocumentSerializer
 
 
-class DocumentCreateView(View):
-    pass
+class DocumentCreateView(generics.CreateAPIView):
+    """
+    Create a new document.
+    """
+
+    lookup_field = 'pk'
+    serializer_class = DocumentSerializer
+
+    def get_queryset(self):
+        return Document.objects.all()
+
 
 
