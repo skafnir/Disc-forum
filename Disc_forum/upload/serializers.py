ea0f7de522545ac2d1935d6df5e6b37af08d6cf4
Jan Sobielarski <jsobielarski@gmail.com>
2019-03-25 12:07:01 +0100

    Document serializer change - get_url added

--- Disc_forum/upload/serializers.py
+++ Disc_forum/upload/serializers.py
@@ -7,17 +7,22 @@ class DocumentSerializer(serializers.ModelSerializer):
     """
     Serializer for Document Model
     """
+    url = serializers.SerializerMethodField(read_only=True)
 
     class Meta:
         model = Document
         fields = [
             'pk',
+            'url',
             'description',
             'document',
             'uploaded_at',
             ]
 
-        read_only_fields = ['pk', 'uploaded_at']
+        read_only_fields = ['pk', 'url', 'uploaded_at']
 
+    def get_url(self, obj):
+        request = self.context.get('request')
+        return obj.get_api_url(request=request)
 
 
