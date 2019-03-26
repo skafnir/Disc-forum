b23944b9f1659fb8c3d0cbaa05ebc39bd0e29458
Jan Sobielarski <jsobielarski@gmail.com>
2019-03-26 19:55:12 +0100

    DocumentRUView url

--- Disc_forum/upload/urls.py
+++ Disc_forum/upload/urls.py
@@ -3,13 +3,14 @@ from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import path, re_path
 
-from upload.views import DocumentCreateView, DocumentListView
+from upload.views import DocumentCreateView, DocumentListView, DocumentRUView
 
 app_name = 'upload'
 
 urlpatterns = [
     path('upload/', DocumentCreateView.as_view(), name='upload'),
     re_path(r'^upload-list/', DocumentListView.as_view(), name='upload-list'),
+    re_path(r'^uploaded/(?P<pk>(\d)+)/$', DocumentRUView.as_view(), name='upload-ru')
 
 ]
 
