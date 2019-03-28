4c73fa709ddbe1fd9a95fc8cba1895774cccc097
Jan Sobielarski <jsobielarski@gmail.com>
2019-03-28 14:57:06 +0100

    Forum app urls include

--- Disc_forum/Disc_forum/urls.py
+++ Disc_forum/Disc_forum/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('upload/', include('upload.urls')),
+    path('forum/', include('forum.urls')),
 ]
