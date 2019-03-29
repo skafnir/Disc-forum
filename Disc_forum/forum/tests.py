05a8aa20129c07002ba07add1903af1251a26911
Jan Sobielarski <jsobielarski@gmail.com>
2019-03-29 13:16:28 +0100

    ForumPostListCreate-APITestCase part I

--- Disc_forum/forum/tests.py
+++ Disc_forum/forum/tests.py
@@ -1,3 +1,25 @@
-from django.test import TestCase
+from django.contrib.auth import get_user_model
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+from forum.models import ForumPost
+
+User = get_user_model()
+
+
+class ForumPostListCreateAPITestCase(APITestCase):
+    def setUp(self):
+        user = User(username='testUser', email='test@test.com')
+        user.set_password('somerandompassword')
+        user.save()
+        forum_post = ForumPost.objects.create(user=user,
+                                              title='Random title',
+                                              content='Lorem ipsum dolor sitt amet',
+                                              )
+
+    def test_single_user(self):
+        user_count = User.objects.count()
+        self.assertEqual(user_count, 1)
+
+    def test_single_forum_post(self):
+        service_count = ForumPost.objects.count()
+        self.assertEqual(service_count, 1)
