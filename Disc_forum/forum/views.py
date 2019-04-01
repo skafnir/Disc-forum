769615415d05fa125d9c0c0a1f08ad418422406d
Jan Sobielarski <jsobielarski@gmail.com>
2019-04-01 14:47:14 +0200

    permission_classes added to ForumPostListCreate and ForumPostRUDView - IsOwnerOrReadOnly

--- Disc_forum/forum/views.py
+++ Disc_forum/forum/views.py
@@ -1,6 +1,8 @@
+from django.db.models import Q
 from rest_framework import generics, mixins
 
 from forum.models import ForumPost
+from forum.permissions import IsOwnerOrReadOnly
 from forum.serializers import ForumPostSerializer
 
 
@@ -10,11 +12,17 @@ class ForumPostListCreateView(mixins.CreateModelMixin, generics.ListAPIView):
     """
     lookup_field = 'pk'
     serializer_class = ForumPostSerializer
+    permission_classes = [IsOwnerOrReadOnly]
 
     # queryset = ForumPost.objects.all()
 
     def get_queryset(self):
-        return ForumPost.objects.all()
+        qs = ForumPost.objects.all()
+        query = self.request.GET.get('q')
+        if query is not None:
+            qs = filter(Q(name__icontains=query) | Q(description_icontains=query)).distinct()
+            # filters qs and allows to check in name and description and makes them unique
+        return qs
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
@@ -29,6 +37,7 @@ class ForumPostRUDView(generics.RetrieveUpdateDestroyAPIView):
     """
     lookup_field = 'pk'
     serializer_class = ForumPostSerializer
+    permission_classes = [IsOwnerOrReadOnly]
 
     def get_queryset(self):
         return ForumPost.objects.all()
