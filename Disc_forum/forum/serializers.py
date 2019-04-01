e135f5ffaa713ddff18f06b6acc74b576fe94d0c
Jan Sobielarski <jsobielarski@gmail.com>
2019-04-01 12:43:38 +0200

    ForumPost Serializer - validation title unique

--- Disc_forum/forum/serializers.py
+++ Disc_forum/forum/serializers.py
@@ -25,3 +25,12 @@ class ForumPostSerializer(serializers.ModelSerializer):
     def get_url(self, obj):
         request = self.context.get('request')
         return obj.get_api_url(request=request)
+
+    def validate_title(self, value):
+        qs = ForumPost.objects.filter(name_iexact=value)
+        if self.instance:                                   # excluding instance
+            qs = qs.exclude(pk=self.instance.pk)
+        if qs.exists():
+            raise serializers.ValidationError("Title already been used")
+        return value
+
