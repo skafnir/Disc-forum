3e834ec7762948be5789fbc752c00e11d9b205be
Jan Sobielarski <jsobielarski@gmail.com>
2019-04-03 16:44:03 +0200

    ForumPost Test rewritten part I

--- Disc_forum/forum/tests.py
+++ Disc_forum/forum/tests.py
@@ -1,26 +1,25 @@
-from django.contrib.auth import get_user_model
-from rest_framework import status, request
-from rest_framework.test import APITestCase, APIClient
+import json
+
+from django.contrib.auth.models import User
+from django.urls import reverse
+
+from rest_framework.test import APITestCase
 from rest_framework.reverse import reverse as api_reverse
 
 from forum.models import ForumPost
 
 
-User = get_user_model()
-
-
 class ForumPostListCreateAPITestCase(APITestCase):
+    url = reverse("forum:forum-post-create")
+
     def setUp(self):
         """
-        Set up - creating user and forums post
+        Set up
         """
-        user = User(username='testUser', email='test@test.com')
-        user.set_password('somerandompassword')
-        user.save()
-        forum_post = ForumPost.objects.create(user=user,
-                                              title='Random title',
-                                              content='Lorem ipsum dolor sitt amet',
-                                              )
+        self.username = "john"
+        self.email = "john@snow.com"
+        self.password = "you_know_nothing"
+        self.user = User.objects.create_user(self.username, self.email, self.password)
 
     def test_single_user(self):
         """
@@ -28,43 +27,19 @@ class ForumPostListCreateAPITestCase(APITestCase):
         """
         user_count = User.objects.count()
         self.assertEqual(user_count, 1)
+    #
+    # def test_create_forum_post(self):
+    #     data = {'title': 'Random title', 'content': 'Lorem ipsum dolor sitt amet'}
+    #     response = self.client.post(self.url, data)
+    #     self.assertEqual(201, response.status_code)
 
-    def test_single_forum_post(self):
+    def test_user_forum_post(self):
         """
-        Test if there is 1 forum post
+        Test to verify user ForumPost list
         """
-        forum_post_count = ForumPost.objects.count()
-        self.assertEqual(forum_post_count, 1)
-
-    def test_get_list(self):
-        """
-        Test for GET list - 200 OK
-        """
-        postings = ForumPost.objects.all()
-        url = api_reverse('forum:forum-post-create')
-        data = {'postings': postings}
-        response = self.client.get(url, data, format='json')
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-    def test_get_item(self):
-        """
-        Test for GET item - 200 OK
-        """
-        posting = ForumPost.objects.first()
-        url = api_reverse('forum:forum-post-create')
-        data = {'posting': posting}
-        response = self.client.get(url, data, format='json')
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-    def test_post_item(self):    # TODO - ValueError: Cannot assign "<django.contrib.auth.models.AnonymousUser object
-        """
-        Test for POST - 200 OK
-        """
-        # user = User.objects.get(username='testUser')
-        url = api_reverse('forum:forum-post-create')
-        data = {'title': 'Some title', 'content': 'Some content'}
-        response = self.client.post(url, data, format='json')
-        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+        ForumPost.objects.create(user=self.user, title='Random title', content='Lorem ipsum dolor sitt amet')
+        response = self.client.get(self.url)
+        self.assertTrue(len(json.loads(response.content)) == ForumPost.objects.count())
 
 
 
