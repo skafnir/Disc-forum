5d62de6962f16d822424ff0adb257d87644fdf4a
Jan Sobielarski <jsobielarski@gmail.com>
2019-04-04 16:35:18 +0200

    ForumPostRUDAPITestCase - get, create, put

--- Disc_forum/forum/tests.py
+++ Disc_forum/forum/tests.py
@@ -112,6 +112,33 @@ class ForumPostRUDAPITestCase(APITestCase):
         user_count = User.objects.count()
         self.assertEqual(user_count, 1)
 
+    def test_get_item(self):
+        """
+        Test for GET item - 200 OK
+        """
+        posting = ForumPost.objects.first()
+        data = {'posting': posting}
+        response = self.client.get(self.url, data)
+        self.assertEqual(response.status_code, 200)
 
+    def test_create_item(self):
+        """
+        Test for POST forum post - 405 method not allowed
+        """
+        self.client.force_login(self.user)
+        data = {'title': 'Random title', 'content': 'Lorem ipsum dolor sitt amet'}
+        response = self.client.post(self.url, data)
+        self.assertEqual(response.status_code, 405)
 
+    def test_put_item(self):
+        """
+        Test for PUT - 200 OK
+        """
+        self.client.force_login(self.user)
+        data = {'title': 'Random title'}
+        response = self.client.put(self.url, data)
+        self.assertEqual(response.status_code, 200)
 
+        response_data = json.loads(response.content)
+        forum_post = ForumPost.objects.get(id=self.forum_post.id)
+        self.assertEqual(response_data.get("title"), forum_post.title)
