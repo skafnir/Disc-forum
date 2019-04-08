7df58fd767682fa8c91dbc5b91b590ed6356669e
Jan Sobielarski <jsobielarski@gmail.com>
2019-04-08 17:46:24 +0200

    Document model changed - user added, uploaded_at renamed to timestamp, owner property added

--- Disc_forum/upload/models.py
+++ Disc_forum/upload/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.db import models
 
 from rest_framework.reverse import reverse as api_reverse
@@ -5,11 +6,17 @@ from rest_framework.reverse import reverse as api_reverse
 
 class Document(models.Model):
     """
-    Model for uploaded documents - description, path to document, upload time
+    Model for uploaded documents - user, description, path to document, upload time
     """
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     description = models.CharField(max_length=255, blank=True)
     document = models.FileField(upload_to='documents/')
-    uploaded_at = models.DateTimeField(auto_now_add=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    # user == owner for permission IsOwnerOrReadOnly
+    @property
+    def owner(self):
+        return self.user
 
     def get_api_url(self, request=None):
         return api_reverse('upload:upload-ru', kwargs={'pk': self.pk}, request=request)
