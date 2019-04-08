08bcce606f6eb0f550b9aa4d3380afde41511c3d
Jan Sobielarski <jsobielarski@gmail.com>
2019-04-08 18:42:19 +0200

    forum urls changed

--- Disc_forum/forum/urls.py
+++ Disc_forum/forum/urls.py
@@ -5,8 +5,8 @@ from forum.views import ForumPostListCreateView, ForumPostRUDView
 app_name = 'forum'
 
 urlpatterns = [
-    path('post-list-create/', ForumPostListCreateView.as_view(), name='forum-post-create'),
-    re_path(r'^post-rud/(?P<pk>(\d)+)/$', ForumPostRUDView.as_view(), name='forum-post-rud'),
+    path('', ForumPostListCreateView.as_view(), name='forum-post-create'),
+    re_path(r'^(?P<pk>(\d)+)/$', ForumPostRUDView.as_view(), name='forum-post-rud'),
 
 ]
 
